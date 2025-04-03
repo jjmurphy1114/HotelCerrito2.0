487070b2db77fab8ba7f5cb3a66a5d6327caa856
ceciherriman <herrimanceci@gmail.com>
2025-04-03 09:10:10 -0300

    screens

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -5,6 +5,9 @@ export default function TabLayout() {
     <Tabs>
       <Tabs.Screen name="index" options={{ title: 'Home' }} />
       <Tabs.Screen name="about" options={{ title: 'About' }} />
+      <Tabs.Screen name="index" options={{ title: 'Home' }} />
+
+      
     </Tabs>
   );
 }
