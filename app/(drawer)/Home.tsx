3c926e2b55c3a8f61d12b8bd962688394af4dec9
jjmurphy1114 <jjmurphy1114@outlook.com>
2025-04-03 11:52:17 -0300

    Installed React Native Paper UI Library

--- app/(drawer)/Home.tsx
+++ app/(drawer)/Home.tsx
@@ -4,28 +4,31 @@ import 'react-native-gesture-handler';
 import { createDrawerNavigator } from '@react-navigation/drawer';
 import Activities from './Activities';
 import Tour from './Tour';
+import { useTheme } from 'react-native-paper';
 
 export default function Home() {
+  const { colors } = useTheme();
+  
   return (
-    <View style={styles.container}>
-          <Text style={styles.text}>HOME PAGE</Text>
+    <View style={{backgroundColor: colors.background}}>
+          <Text style={{color: colors.primary}}>HOME PAGE</Text>
         </View>
       );
     }
 
-    const styles = StyleSheet.create({
-      container: {
-        flex: 1,
-        backgroundColor: '#25292e',
-        alignItems: 'center',
-        justifyContent: 'center',
-      },
-      text: {
-        color: '#fff',
-      },
-      button: {
-        fontSize: 20,
-        textDecorationLine: 'underline',
-        color: '#fff',
-      },
-});
+//     const styles = StyleSheet.create({
+//       container: {
+//         flex: 1,
+//         backgroundColor: '#25292e',
+//         alignItems: 'center',
+//         justifyContent: 'center',
+//       },
+//       text: {
+//         color: '#fff',
+//       },
+//       button: {
+//         fontSize: 20,
+//         textDecorationLine: 'underline',
+//         color: '#fff',
+//       },
+// });
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,5 +1,24 @@
 import { Slot } from 'expo-router';
+import { MD3LightTheme as DefaultTheme, PaperProvider } from 'react-native-paper';
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: '#FFFFFF',
+    primary: '#2E7E41',       // green
+    secondary: '#E79F30',     // orange-yellow
+    onPrimary: '#FFFFFF',
+    onSecondary: '#000000',
+    surface: '#FFFFFF',
+    onSurface: '#000000',
+  },
+};
 
 export default function RootLayout() {
-  return <Slot />;
+  return (
+    <PaperProvider theme={theme}>
+      <Slot />
+    </PaperProvider>
+  )
 }
