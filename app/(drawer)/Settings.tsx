d68227f3f761baac3b5f3b93a992cc4c8e228f97
jjmurphy1114 <jjmurphy1114@outlook.com>
2025-04-07 09:20:10 -0300

    Added language toggle in settings

--- app/(drawer)/Settings.tsx
+++ app/(drawer)/Settings.tsx
@@ -2,6 +2,7 @@ import { useTheme } from "react-native-paper";
 import { useTranslation } from "react-i18next";
 import { View } from "react-native";
 import { Text } from "react-native-paper"
+import LanguageToggle from "../components/LanguageToggle";
 
 export default function Settings(){
     const { colors } = useTheme();
@@ -10,6 +11,7 @@ export default function Settings(){
     return (
         <View style={{backgroundColor: colors.background, alignItems: "center"}}>
             <Text variant="headlineSmall">This will let you change language</Text>
+            <LanguageToggle/>
         </View>
     )
 }
