b4ed0be1afae852fd2ca8920b01af9b377f53fc1
ceciherriman <herrimanceci@gmail.com>
2025-04-08 10:53:24 -0300

    moved cerrito logo

--- app/components/CerritoLogo.tsx
+++ app/components/CerritoLogo.tsx
@@ -1,4 +1,5 @@
 import { Image } from "expo-image";
+import { StyleSheet} from 'react-native';
 
 type Props = {
   width?: number;
@@ -9,10 +10,17 @@ export default function CerritoLogo({ width = 40, height = 40 }: Props) {
   const blurhash =
     '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
 
+  const styles = StyleSheet.create({
+    cerritoImage: {
+      width: width,
+      height: height,
+      marginBottom: 15
+    }
+  });
   return (
     <Image
       source={require('../../assets/images/hotel-cerrito-logo.png')}
-      style={{ width, height }}
+      style={styles.cerritoImage}
     //   placeholder={{ blurhash }}
       contentFit="contain"
       transition={500}
