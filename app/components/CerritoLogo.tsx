493f7df83b2ba791717366618a92bff1bbba230a
ceciherriman <herrimanceci@gmail.com>
2025-04-08 11:08:13 -0300

    used window dimensions for logo placement

--- app/components/CerritoLogo.tsx
+++ app/components/CerritoLogo.tsx
@@ -1,23 +1,25 @@
 import { Image } from "expo-image";
-import { StyleSheet} from 'react-native';
+import { StyleSheet, Dimensions} from 'react-native';
 import { Platform } from 'react-native';
 
 type Props = {
-  width?: number;
-  height?: number;
+  widthProp?: number;
+  heightProp?: number;
 };
 
-export default function CerritoLogo({ width = 40, height = 40 }: Props) {
+const { width, height } = Dimensions.get('window');
+
+export default function CerritoLogo({ widthProp = width*.13, heightProp = height * .06 }: Props) {
   const blurhash =
     '|rF?hV%2WCj[ayj[a|j[az_NaeWBj@ayfRayfQfQM{M|azj[azf6fQfQfQIpWXofj[ayj[j[fQayWCoeoeaya}j[ayfQa{oLj?j[WVj[ayayj[fQoff7azayj[ayj[j[ayofayayayj[fQj[ayayj[ayfjj[j[ayjuayj[';
-
+  
   const styles = StyleSheet.create({
     cerritoImage: {
-      width: width,
-      height: height,
+      width: widthProp,
+      height: heightProp,
       marginBottom: Platform.select({
         android: 0,
-        ios: 12,
+        ios: height * .015,
       })
     }
   });
