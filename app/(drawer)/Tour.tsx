91f48f790f11212d1da5b961408939662ff2b085
ceciherriman <herrimanceci@gmail.com>
2025-04-10 11:28:46 -0300

    back button from self-guided tour start

--- app/(drawer)/Tour.tsx
+++ app/(drawer)/Tour.tsx
@@ -1,17 +1,50 @@
-import { View, StyleSheet, ScrollView } from 'react-native';
-import { Text, Button, Card, useTheme } from 'react-native-paper';
+import { View, StyleSheet, ScrollView, TouchableOpacity, Platform, Dimensions } from 'react-native';
+import { Text, Button, Card, useTheme, IconButton } from 'react-native-paper';
 import { useRouter } from 'expo-router';
 import { useTranslation } from 'react-i18next';
 import { Image } from 'expo-image';
 import ImageCarousel from '../components/ImageCarousel';
 
+const { width, height } = Dimensions.get('window');
+const imageSizeWidth = width * 0.4;
+
 export default function TourStart() {
   const router = useRouter();
   const { t } = useTranslation();
   const { colors } = useTheme();
+  
+  /*For navigating to the previous page*/
+  const PrevPage = () => router.replace('/(drawer)/Home');
 
   const styles = StyleSheet.create({
     container: { flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20, textAlign: 'center' },
+    header: {
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          backgroundColor: colors.secondary,
+          paddingVertical: 0,
+          justifyContent: 'center',
+          alignItems: 'center',
+        },
+        headerText: {
+          color: colors.onSecondary, 
+          textAlign: 'center',
+          fontSize: 20,
+          padding: width*.008,
+          fontFamily: Platform.select({
+            android: 'Inter_900Black',
+            ios: 'Inter-Black',
+          }),
+          position: 'absolute',
+            left: 0,
+            right: 0,
+        },
+        backButton: {
+          alignSelf: 'flex-start',
+          zIndex: 10
+        },
     card: {
       margin: 20,
       padding: 10,
@@ -35,6 +68,13 @@ export default function TourStart() {
 
   return (
     <View style={styles.container}>
+      <View style={styles.header}>
+          <TouchableOpacity onPress={PrevPage} style={styles.backButton}>
+              <IconButton icon="arrow-left" size={20} />
+          </TouchableOpacity>
+          <Text style={styles.headerText}>{t('tour.header')}</Text>
+        </View>
+
       <ImageCarousel
         images={[
           require('../../assets/images/HotelWithTrees.jpg'),
