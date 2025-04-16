18537d7ceebf044e30d87fba8555e505d4d235a2
jjmurphy1114 <jjmurphy1114@outlook.com>
2025-04-16 09:52:55 -0300

    Welcome page

--- app/index.tsx
+++ app/index.tsx
@@ -1,9 +1,10 @@
-import { View, Text, ActivityIndicator, StyleSheet, Platform } from 'react-native';
+import { View, Text, ActivityIndicator, StyleSheet, Platform, Dimensions } from 'react-native';
 import { useTheme, Button } from 'react-native-paper';
 import { useRouter } from 'expo-router';
 import { useTranslation } from 'react-i18next';
 import { useEffect, useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Image } from 'expo-image';
 import React from 'react';
 
 const LANGUAGE_KEY = 'user-language';
@@ -14,11 +15,17 @@ export default function Welcome() {
   const [loading, setLoading] = useState(true);
   const { colors } = useTheme();
 
+   // Expo Image setup
+  const { width, height } = Dimensions.get('window');
+  const imageSizeWidth = width * 0.9;
+  const imageSizeHeight = height * 0.4;
+
   const styles = StyleSheet.create({ 
     container: {
       flex: 1, 
       justifyContent: 'center',
-      alignItems: 'center'
+      alignItems: 'center',
+      backgroundColor: colors.background
     },
     text: {
       fontSize: 24
@@ -29,9 +36,30 @@ export default function Welcome() {
       width: '90%',
       marginTop: 10,
     },
-    directionsButton: {
-      backgroundColor: colors.primary
-    }
+    spanishButton: {
+      backgroundColor: colors.primary,
+      margin: 10,
+      width: '70%', // Ensures both buttons have the same width
+      height: '6%',
+      alignSelf: 'center',
+      justifyContent: 'center', // Centers text vertically
+      alignItems: 'center', // Centers text horizontally
+    },
+    englishButton: {
+      backgroundColor: colors.secondary,
+      margin: 10,
+      width: '70%', // Ensures both buttons have the same width
+      height: '6%',
+      alignSelf: 'center',
+      justifyContent: 'center', // Centers text vertically
+      alignItems: 'center', // Centers text horizontally
+    },
+    image: {
+      width: imageSizeWidth,
+      height: imageSizeHeight,
+      backgroundColor: colors.background,
+      margin: 10
+    },
   })
 
   useEffect(() => {
@@ -60,7 +88,7 @@ export default function Welcome() {
   };
 
   // Uncomment to clear language
-  AsyncStorage.removeItem('user-language');
+  // AsyncStorage.removeItem('user-language');
 
   if (loading) {
     return (
@@ -68,25 +96,37 @@ export default function Welcome() {
         <ActivityIndicator size="large" />
       </View>
     );
-  }
+  }  
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text style={{ fontSize: 24 }}>Welcome!</Text>
-      <Button onPress={() => handleLanguageSelect("en")}> Continue in English </Button>
-      <Button
-          style={styles.directionsButton}
-          labelStyle={{
-            fontFamily: Platform.select({
-              android: 'Inter_500Medium',
-              ios: 'Inter-Medium',
-            }),
-            fontSize: 16, // optional
-          }}
-          mode="contained"
-        > button
-        </Button>
-      <Button onPress={() => handleLanguageSelect("es")}> Continuar en Español </Button>
+    <View style={{ flex: 1, justifyContent: 'flex-start', alignItems: 'center', backgroundColor: colors.background, }}>
+      <View style={{width: '100%', height: '100%', marginTop: '26%'}}>
+      <Image source={require('../assets/images/cerrito-logo.png')} 
+            style={styles.image}
+            contentFit="cover"
+            transition={1000}
+      />
+      <Button onPress={() => handleLanguageSelect("es")}
+        style={styles.spanishButton}
+        labelStyle={{
+          fontFamily: Platform.select({
+            android: 'Inter_500Medium',
+            ios: 'Inter-Medium',
+          }),
+          fontSize: 18, // optional
+        }}
+        mode="contained"> Continuar en Español </Button>
+      <Button onPress={() => handleLanguageSelect("en")}
+        style={styles.englishButton}
+        labelStyle={{
+          fontFamily: Platform.select({
+            android: 'Inter_500Medium',
+            ios: 'Inter-Medium',
+          }),
+          fontSize: 18, // optional
+        }}
+        mode="contained"> Continue in English </Button>   
+      </View>
     </View>
   );
 }
